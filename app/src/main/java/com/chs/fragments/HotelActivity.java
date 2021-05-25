ae15a5a4bb2fa3ad8f8d4b12822bf6d0044da7d5
Carlos Henrique <42853215+H3nr1q@users.noreply.github.com>
2021-05-25 17:37:27 -0300

    Atualizando

--- app/src/main/java/com/chs/fragments/HotelActivity.java
+++ app/src/main/java/com/chs/fragments/HotelActivity.java
@@ -1,6 +1,8 @@
 package com.chs.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,8 +17,20 @@ public class HotelActivity extends AppCompatActivity implements HotelListFragmen
 
     @Override
     public void clicouNoHotel(Hotel hotel) {
-        Intent it = new Intent(this, HotelDetalheActivity.class);
-        it.putExtra(HotelDetalheActivity.EXTRA_HOTEL,hotel);
-        startActivity(it);
+        if (isTablet()) {
+            HotelDetalheFragment fragment = HotelDetalheFragment.novaInstancia(hotel);
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.detalheT, fragment, HotelDetalheFragment.TAG_DETALHE);
+            ft.commit();
+        } else {
+            Intent it = new Intent(this, HotelDetalheActivity.class);
+            it.putExtra(HotelDetalheActivity.EXTRA_HOTEL, hotel);
+            startActivity(it);
+        }
+    }
+
+    private boolean isTablet() {
+        return findViewById(R.id.detalheT)!=null;
     }
 }
